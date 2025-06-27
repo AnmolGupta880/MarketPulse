e50915cdc80eedcc1a4326cc7d9fe444b00b2f07
ANMOL GUPTA <161852762+AnmolGupta880@users.noreply.github.com>
2025-06-27 17:38:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './components/navbar';
-import StockDashboard from './pages/StockDashboard';
+import StockDashboard from './pages/stockDashboard';
 import Footer from './components/footer';
 
 function App() {
